ff5a032875b9b3e9db5ef134688200619b4b7bd5
vivianroshan <vivianroshan@gmail.com>
2023-09-15 20:10:19 -0700

    commit changed to run on docker

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-./bin/html.sh resume.md resume.html
-./bin/pdf.sh resume.html resume.pdf
+script_parent_dirname="$(dirname "$(readlink -f "$0")")"
+$script_parent_dirname/bin/html.sh resume.md resume.html
+$script_parent_dirname/bin/pdf.sh resume.html resume.pdf
